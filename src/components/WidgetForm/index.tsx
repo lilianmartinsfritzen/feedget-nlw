84cd2a9ae418eadfaf1d84f7e2a6ac7bcea232fb
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-07 00:16:18 -0300

    feat: component feedback content step

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 import { CloseButton } from "../CloseButton";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 import bugImageUrl from "../../assets/images/bug.svg";
 import ideaImageUrl from "../../assets/images/idea.svg";
 import thoughtImageUrl from "../../assets/images/thought.svg";
-import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -36,19 +37,19 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  function handleRestartFeedback() {
+    setFeedbackType(null)
+  }
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">
-          Deixe seu feedback
-          <CloseButton />
-        </span>
-      </header>
 
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello World</p>
+        <FeedbackContentStep 
+          feedbackType={feedbackType} 
+          onFeeckbackRestartRequested={handleRestartFeedback}  
+        />
       )}
 
       <footer className="text-xs text-neutral-400">
